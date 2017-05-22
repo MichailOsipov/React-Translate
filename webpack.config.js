6189a86f86df826667b8419358cfe2b831c13975
Michail <osimike96@gmail.com>
2017-05-22 19:40:27 +0800

    added package.json, webpack.config, gitignore

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+		path: __dirname + '/public',
+        filename: 'bundle.js'
+    },
+    devtool: NODE_ENV == 'development' ? 'source-map' : null,
+    module: {
+        loaders: [
+            {
+				test: /.js$/,
+				loader: 'babel-loader',
+				exclude: /node_modules/,
+				query: {
+				presets: ['es2015', 'react', 'stage-2']
+				}
+			},
+            {
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader!resolve-url!sass-loader?sourceMap')
+            },
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+            },
+            {
+                test: /\.woff2?$|\.ttf$|\.eot$|\.svg$|\.png|\.jpe?g|\.gif$/,
+                loader: 'file-loader'
+            }
+        ]
+    },
+	devServer: {
+        host: 'localhost',
+        port: 8080,
+        contentBase: __dirname + '/public'
+	},
+    plugins: [
+        new ExtractTextPlugin('styles.css', {
+            allChunks: true
+        })
+    ]
+};
